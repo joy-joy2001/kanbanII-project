5759f5816b4380a29527933b5c01a639c79c76db
Joy E <jefiannayi@gmail.com>
2022-05-28 08:41:33 +0100

    make form dynamic

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("newtask-form").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("newtask-form").style.display = "none";
+}
